4b7af05bf2b5c921e91b289df3a568f56cf46145
Tri Ardini <tr.ardn@gmail.com>
2020-07-17 21:11:43 +0700

    handle event chat on server

--- ws/chatroom.ts
+++ ws/chatroom.ts
@@ -1,14 +1,23 @@
-import { WebSocket } from "https://deno.land/std/ws/mod.ts";
+import { WebSocket, isWebSocketCloseEvent } from "https://deno.land/std/ws/mod.ts";
 import { v4 } from "https://deno.land/std/uuid/mod.ts";
 
 let sockets = new Map<string, WebSocket>();
 
 const chatConnection = async (ws: WebSocket) => {
-    console.log('new websocket connection')
-
     // add new websocket connection to map
     const uid = v4.generate()
     sockets.set(uid, ws)
+
+    for await (const event of ws) {
+        // delete socket if connection closed
+        if (isWebSocketCloseEvent(event)) {
+            sockets.delete(uid)
+        }
+
+        if (typeof event === 'string') {
+            let eventObject = JSON.parse(event)
+        }
+    }
 }
 
 export { chatConnection };
